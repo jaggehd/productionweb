7aa3ea602252d3054bc3f68ec7e5a29525210809
JC <jc.gagnon1@hotmail.com>
2020-10-14 18:13:27 -0400

    commit html/scss/js

--- src/js/script.js
+++ src/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     //Un commentaire
     console.log("Ça fonctionne!!!");
 
+
     function scrollWaypointInit( items, trigger ) {
         items.each( function() {
             var element = $(this),
@@ -26,13 +27,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         });
     }
 
-//Call the init
+    const menu = document.querySelector("#toggle");
+    if(menu !== null) {
+        menu.addEventListener("click", (e) => {
+            menu.classList.toggle("active");
+            document.querySelector("#overlay").classList.toggle("open");
+        });
+    }
 
-    $(document).ready(function(){
+    scrollWaypointInit( $('.animateMe') );
 
-        scrollWaypointInit( $('.animateMe') );
 
-    });
+
+
 
 
 });
